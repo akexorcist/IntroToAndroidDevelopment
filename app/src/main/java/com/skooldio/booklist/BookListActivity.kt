d7dd954981229cf2523d64c85ad8b681209fdcc7
Akexorcist <akexorcist@gmail.com>
2019-11-23 07:30:37 +0700

    Remove unused package name

--- app/src/main/java/com/skooldio/booklist/BookListActivity.kt
+++ app/src/main/java/com/skooldio/booklist/BookListActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.skooldio.booklist.vo.Book
 import kotlinx.android.synthetic.main.activity_book_list.*
 
 class BookListActivity : AppCompatActivity() {
